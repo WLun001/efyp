0ea03bc6054c54ebcad119ce9242d91e1960bb96
Wei Lun <weilun_95@hotmail.com>
2017-10-24 23:15:46 +0800

    change db credential

--- yii2/config/db.php
+++ yii2/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=id2512886_fypportal',
-    'username' => 'id2512886_fypportal',
-    'password' => 'admin',
+    'dsn' => 'mysql:host=localhost;dbname=efyp',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
